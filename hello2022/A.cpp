d6cef7e69cec58b7c97cb88bb155876d5c6a129c
pranab <kumarpranab091@gmail.com>
2022-01-05 01:34:35 +0530

    Stable Arrangement of Rooks

--- hello2022/A.cpp
+++ hello2022/A.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
+int t;
+cin>>t;
+while(t--){
+    int n,k;
+    cin>>n>>k;
+    if((n+1)/2 <k){
+        cout<<"-1\n";
+        continue;
+    }
+    vector<string> s(n,string(n,'.'));
+    for(int i=0;i<k;i++)
+        s[2*i][2*i]='R';
+        for(int i=0;i<n;i++)
+            cout<<s[i]<<"\n";
+        
+    
+    
+}
 return 0;
 }
