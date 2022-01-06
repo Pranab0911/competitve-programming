b8daaa9ed3a27e3574c98a9a8979ea9c1f2b36b2
pranab <kumarpranab091@gmail.com>
2022-01-07 01:00:34 +0530

    helpfulmaths

--- helpfulmaths.cpp
+++ helpfulmaths.cpp
@@ -1,17 +1,20 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-string s;
-cin>>s;
-vector<int> v;
-for(int i=0;i<s.size();i++){
-if(s[i]!='+'){
-    v.push_back(s[i]);
-}
-}
-sort(v.begin(),v.end());
-for(int i=0;i<v.size();i++){
-    cout<<v[i];
-}
-return 0;
+    string s;
+    cin>>s;
+    int arr[1000];
+    int c=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='+') continue;
+        else
+        arr[c++]=s[i]-'0';
+    }
+    sort(arr,arr+c);
+    for(int i=0;i<c;i++){
+        cout<<arr[i];
+        if(i==c-1 ) break;
+        cout<<"+";
+    }
 }
